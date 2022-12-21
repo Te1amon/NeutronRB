4ab5904a2f0f788ed94ec7bc49084c384c85266e
Te1amon <72522683+Te1amon@users.noreply.github.com>
2022-12-21 06:29:04 -0500

    formatted code

--- Serverside.lua
+++ Serverside.lua
@@ -1,31 +1,33 @@
 if game.ReplicatedStorage.Events then
     -- folder already exists
-      else
-      -- create a new folder
-      lol = Instance.new("Folder")
-      lol.Name = "Events"
-      lol.Parent = game.ReplicatedStorage
-      end
+else
+    -- create a new folder
+    lol = Instance.new("Folder")
+    lol.Name = "Events"
+    lol.Parent = game.ReplicatedStorage
+end
 
 if game.ReplicatedStorage.Events.NeutronEvent then
-      -- remote already exists
-      else
-      -- create a new remoteevent
-lol = Instance.new("RemoteEvent")
-lol.Name = "NeutronEvent"
-lol.Parent = game.ReplicatedStorage.Events
-      end
+    -- remote already exists
+else
+    -- create a new remoteevent
+    lol = Instance.new("RemoteEvent")
+    lol.Name = "NeutronEvent"
+    lol.Parent = game.ReplicatedStorage.Events
+end
 
-      wait()
+wait()
 
-game.ReplicatedStorage.Events.NeutronEvent.OnServerEvent:Connect(function(plr, oldpos, newpos)
-getCharacter = function(plrmabob)
-      if plrmabob.Character:FindFirstChild("HumanoidRootPart") then
-        return plrmabob
+game.ReplicatedStorage.Events.NeutronEvent.OnServerEvent:Connect(
+    function(plr, oldpos, newpos)
+        getCharacter = function(plrmabob)
+            if plrmabob.Character:FindFirstChild("HumanoidRootPart") then
+                return plrmabob
+            end
         end
-      end
-    char = getCharacter(plr)
-    if newpos.Y - oldpos.Y > 10 then -- lets see if this works lol
-      char:MoveTo(oldpos)
-      end
-    end)
+        char = getCharacter(plr)
+        if newpos.Y - oldpos.Y > 10 then -- lets see if this works lol
+            char:MoveTo(oldpos)
+        end
+    end
+)
